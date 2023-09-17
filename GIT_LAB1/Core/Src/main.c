f28c1031116815919e908e1a0d0c7f4b8c3c0bfd
TRUNG <trung.tran24@hcmut.edu.vn>
2023-09-18 00:55:41 +0700

    audit

--- GIT_LAB1/Core/Src/main.c
+++ GIT_LAB1/Core/Src/main.c
@@ -50,18 +50,18 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 /* USER CODE BEGIN PFP */
 void clearAllClock(){
-	HAL_GPIO_WritePin(LED_12_GPIO_Port, LED_12_Pin, SET);
-	HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin, SET);
-	HAL_GPIO_WritePin(LED_10_GPIO_Port, LED_10_Pin, SET);
-	HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin, SET);
-	HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin, SET);
-	HAL_GPIO_WritePin(LED_7_GPIO_Port, LED_7_Pin, SET);
-	HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_6_Pin, SET);
-	HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin, SET);
-	HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin, SET);
-	HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin, SET);
-	HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin, SET);
-	HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, SET);
+	HAL_GPIO_WritePin(LED_12_GPIO_Port, LED_12_Pin, RESET);
+	HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin, RESET);
+	HAL_GPIO_WritePin(LED_10_GPIO_Port, LED_10_Pin, RESET);
+	HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin, RESET);
+	HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin, RESET);
+	HAL_GPIO_WritePin(LED_7_GPIO_Port, LED_7_Pin, RESET);
+	HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_6_Pin, RESET);
+	HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin, RESET);
+	HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin, RESET);
+	HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin, RESET);
+	HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin, RESET);
+	HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, RESET);
 }
 /* USER CODE END PFP */
 
