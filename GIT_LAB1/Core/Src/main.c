10e69da26e8a7af38d1f3de9368c641f6b946a60
TRUNG <trung.tran24@hcmut.edu.vn>
2023-09-17 23:46:33 +0700

    init for ex7

--- GIT_LAB1/Core/Src/main.c
+++ GIT_LAB1/Core/Src/main.c
@@ -49,7 +49,20 @@
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 /* USER CODE BEGIN PFP */
-
+void clearAllClock(){
+	HAL_GPIO_WritePin(LED_12_GPIO_Port, LED_12_Pin, SET);
+	HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin, SET);
+	HAL_GPIO_WritePin(LED_10_GPIO_Port, LED_10_Pin, SET);
+	HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin, SET);
+	HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin, SET);
+	HAL_GPIO_WritePin(LED_7_GPIO_Port, LED_7_Pin, SET);
+	HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_6_Pin, SET);
+	HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin, SET);
+	HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin, SET);
+	HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin, SET);
+	HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin, SET);
+	HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, SET);
+}
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -95,78 +108,30 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-
+	switch(counter){
+	case 0:
+		clearAllClock();
+		counter=1;
+		break;
+	case 1:
+		HAL_GPIO_TogglePin(LED_12_GPIO_Port, LED_12_Pin);
+		HAL_GPIO_TogglePin(LED_11_GPIO_Port, LED_11_Pin);
+		HAL_GPIO_TogglePin(LED_10_GPIO_Port, LED_10_Pin);
+		HAL_GPIO_TogglePin(LED_9_GPIO_Port, LED_9_Pin);
+		HAL_GPIO_TogglePin(LED_8_GPIO_Port, LED_8_Pin);
+		HAL_GPIO_TogglePin(LED_7_GPIO_Port, LED_7_Pin);
+		HAL_GPIO_TogglePin(LED_6_GPIO_Port, LED_6_Pin);
+		HAL_GPIO_TogglePin(LED_5_GPIO_Port, LED_5_Pin);
+		HAL_GPIO_TogglePin(LED_4_GPIO_Port, LED_4_Pin);
+		HAL_GPIO_TogglePin(LED_3_GPIO_Port, LED_3_Pin);
+		HAL_GPIO_TogglePin(LED_2_GPIO_Port, LED_2_Pin);
+		HAL_GPIO_TogglePin(LED_1_GPIO_Port, LED_1_Pin);
+		counter=0;
+		break;
+	}
     /* USER CODE BEGIN 3 */
 
-
-	  switch(counter){
-	  case 12:
-	 		  HAL_GPIO_TogglePin(LED_12_GPIO_Port, LED_12_Pin);
-	 		  HAL_GPIO_TogglePin(LED_10_GPIO_Port, LED_10_Pin);
-	 		  break;
-	  case 11:
-	  		  HAL_GPIO_TogglePin(LED_11_GPIO_Port, LED_11_Pin);
-	  		  HAL_GPIO_TogglePin(LED_10_GPIO_Port, LED_10_Pin);
-	  		  break;
-	  case 10:
-	  		  HAL_GPIO_TogglePin(LED_10_GPIO_Port, LED_10_Pin);
-	  		  HAL_GPIO_TogglePin(LED_9_GPIO_Port, LED_9_Pin);
-	  		  break;
-	  case 9:
-	  		  HAL_GPIO_TogglePin(LED_9_GPIO_Port, LED_9_Pin);
-	  		  HAL_GPIO_TogglePin(LED_8_GPIO_Port, LED_8_Pin);
-	  		  break;
-	  case 8:
-	 		  HAL_GPIO_TogglePin(LED_8_GPIO_Port, LED_8_Pin);
-	 		  HAL_GPIO_TogglePin(LED_7_GPIO_Port, LED_7_Pin);
-	 		  break;
-	  case 7:
-	  		  HAL_GPIO_TogglePin(LED_7_GPIO_Port, LED_7_Pin);
-	  		  HAL_GPIO_TogglePin(LED_6_GPIO_Port, LED_6_Pin);
-	  		  break;
-	  case 6:
-	 		  HAL_GPIO_TogglePin(LED_6_GPIO_Port, LED_6_Pin);
-	 		  HAL_GPIO_TogglePin(LED_5_GPIO_Port, LED_5_Pin);
-	 		  break;
-	  case 5:
-	 		  HAL_GPIO_TogglePin(LED_5_GPIO_Port, LED_5_Pin);
-	 		  HAL_GPIO_TogglePin(LED_4_GPIO_Port, LED_4_Pin);
-	 		  break;
-	  case 4:
-	 		  HAL_GPIO_TogglePin(LED_4_GPIO_Port, LED_4_Pin);
-	 		  HAL_GPIO_TogglePin(LED_3_GPIO_Port, LED_3_Pin);
-	 		  break;
-	  case 3:
-			  HAL_GPIO_TogglePin(LED_3_GPIO_Port, LED_3_Pin);
-			  HAL_GPIO_TogglePin(LED_2_GPIO_Port, LED_2_Pin);
-			  break;
-	  case 2:
-	 		  HAL_GPIO_TogglePin(LED_2_GPIO_Port, LED_2_Pin);
-	 		  HAL_GPIO_TogglePin(LED_1_GPIO_Port, LED_1_Pin);
-	 		  break;
-	  case 1:
-	 		  HAL_GPIO_TogglePin(LED_1_GPIO_Port, LED_1_Pin);
-	 		  HAL_GPIO_TogglePin(LED_12_GPIO_Port, LED_12_Pin);
-	 		  break;
-	  default:
-		  	  HAL_GPIO_TogglePin(LED_11_GPIO_Port, LED_11_Pin);
-		  	  HAL_GPIO_TogglePin(LED_10_GPIO_Port, LED_10_Pin);
-		  	  HAL_GPIO_TogglePin(LED_9_GPIO_Port, LED_9_Pin);
-		  	  HAL_GPIO_TogglePin(LED_8_GPIO_Port, LED_8_Pin);
-		  	  HAL_GPIO_TogglePin(LED_7_GPIO_Port, LED_7_Pin);
-		  	  HAL_GPIO_TogglePin(LED_6_GPIO_Port, LED_6_Pin);
-		  	  HAL_GPIO_TogglePin(LED_5_GPIO_Port, LED_5_Pin);
-		  	  HAL_GPIO_TogglePin(LED_4_GPIO_Port, LED_4_Pin);
-		  	  HAL_GPIO_TogglePin(LED_3_GPIO_Port, LED_3_Pin);
-		  	  HAL_GPIO_TogglePin(LED_2_GPIO_Port, LED_2_Pin);
-		  	  HAL_GPIO_TogglePin(LED_1_GPIO_Port, LED_1_Pin);
-		  	  break;
-	  }
-	  if(counter>=12) counter=1;
-	  else counter++;
 	  HAL_Delay(1000);
-
-
   /* USER CODE END 3 */
 }
 }
