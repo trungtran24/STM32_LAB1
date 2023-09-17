7f90e566238593f6a6bb8cb07d7551ce2b3d07ec
TRUNG <trung.tran24@hcmut.edu.vn>
2023-09-18 00:25:29 +0700

    init for ex9

--- GIT_LAB1/Core/Src/main.c
+++ GIT_LAB1/Core/Src/main.c
@@ -105,7 +105,46 @@ void setNumberOnClock(int num){
 	}
 }
 /* USER CODE END PFP */
-
+void clearNumberOnClock(int number) {
+	switch (number) {
+	case 0:
+		HAL_GPIO_WritePin(LED_1_GPIO_Port, LED_1_Pin, SET);
+		break;
+	case 1:
+		HAL_GPIO_WritePin(LED_2_GPIO_Port, LED_2_Pin, SET);
+		break;
+	case 2:
+		HAL_GPIO_WritePin(LED_3_GPIO_Port, LED_3_Pin, SET);
+		break;
+	case 3:
+		HAL_GPIO_WritePin(LED_4_GPIO_Port, LED_4_Pin, SET);
+		break;
+	case 4:
+		HAL_GPIO_WritePin(LED_5_GPIO_Port, LED_5_Pin, SET);
+		break;
+	case 5:
+		HAL_GPIO_WritePin(LED_6_GPIO_Port, LED_6_Pin, SET);
+		break;
+	case 6:
+		HAL_GPIO_WritePin(LED_7_GPIO_Port, LED_7_Pin, SET);
+		break;
+	case 7:
+		HAL_GPIO_WritePin(LED_8_GPIO_Port, LED_8_Pin, SET);
+		break;
+	case 8:
+		HAL_GPIO_WritePin(LED_9_GPIO_Port, LED_9_Pin, SET);
+		break;
+	case 9:
+		HAL_GPIO_WritePin(LED_10_GPIO_Port, LED_10_Pin, SET);
+		break;
+	case 10:
+		HAL_GPIO_WritePin(LED_11_GPIO_Port, LED_11_Pin, SET);
+		break;
+	case 11:
+		HAL_GPIO_WritePin(LED_12_GPIO_Port, LED_12_Pin, SET);
+		break;
+	}
+}
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
@@ -161,11 +200,12 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
-	  if(counter>12) counter=1;
-	  	else counter++;
+	  counter = 1;
     /* USER CODE BEGIN 3 */
 	setNumberOnClock(counter-1);
-	  HAL_Delay(1000);
+	  HAL_Delay(5000);
+	  clearNumberOnClock(counter-1);
+	  HAL_Delay(5000);
   /* USER CODE END 3 */
 }
 }
